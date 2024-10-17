f4a7f0124743ff14434635b627d1932689e9e4d3
Ying Li <liying.cn.2010@gmail.com>
2024-10-17 23:24:30 +0900

    fix `last-modified` in HTML files

---
+++ devtools/FixLastModified.main.kts
@@ -0,0 +1,38 @@
+#!/usr/bin/env kotlin
+
+@file:ScriptFileLocation("scriptFile")
+
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.text.Charsets.UTF_8
+
+val topFolder: File = scriptFile.absoluteFile.resolve("../..").normalize()
+println("topFolder: $topFolder")
+
+val buildFolder: File = topFolder.absoluteFile.resolve("build").normalize()
+println("buildFolder: $buildFolder")
+
+val today = Date()
+val dateStr = SimpleDateFormat("yyyy/MM/dd").format(today)
+val oldHtmlCode = "<div class=\"last-modified\">[^<]*</div>".toRegex()
+val newHtmlCode = "<div class=\"last-modified\">最终更新: $dateStr</div>"
+
+buildFolder.walkTopDown().forEach { htmlFile ->
+    fixLastModified(htmlFile)
+}
+
+fun fixLastModified(htmlFile: File) {
+    if (!htmlFile.isFile) {
+        return
+    }
+
+    if (htmlFile.extension != "html") {
+        return
+    }
+
+    val oldText = htmlFile.readText(charset = UTF_8)
+    val newText = oldHtmlCode.replace(oldText, newHtmlCode)
+
+    htmlFile.writeText(newText, charset = UTF_8)
+}
